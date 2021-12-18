14202332d4b929a90479fb7e6841aa4e5b535fe5
raptorswing <971991+raptorswing@users.noreply.github.com>
2021-12-18 15:11:24 -0800

    Move DHT stuff into its own sub module

---
+++ src/dht/dht_settings.rs
@@ -0,0 +1,75 @@
+pub struct DHTSettings {
+    /// Number of bytes for token secrets for get_peers responses
+    pub token_secret_size: usize,
+
+    /// Max number of peers to provide in response to a get_peers.
+    /// Shouldn't be much higher than this as the entire response packet needs to be less than 1500
+    pub max_peers_response: usize,
+
+    /// Max number of info hashes to provide in response to a sample_infohashes request
+    pub max_sample_response: usize,
+
+    /// How often we claim to rotate our sample_infohashes response
+    pub min_sample_interval_secs: i32,
+
+    /// We'll ping the "routers" at least this often (we may ping more frequently if needed)
+    pub router_ping_interval_secs: u64,
+
+    /// We'll ping previously-verified nodes at least this often to re-verify them
+    pub reverify_interval_secs: u64,
+
+    /// Verified nodes that we don't reverify within this amount of time are dropped
+    pub reverify_grace_period_secs: u64,
+
+    /// New nodes have this long to respond to a ping before we drop them
+    pub verify_grace_period_secs: u64,
+
+    /// When asked to provide peers, we'll only provide ones that announced within this time
+    pub get_peers_freshness_secs: u64,
+
+    /// We'll think about sending a find_nodes request at least this often.
+    /// If we have enough nodes already we might not do it.
+    pub find_nodes_interval_secs: u64,
+
+    /// We won't send a periodic find_nodes request if we have at least this many unverified nodes
+    pub find_nodes_skip_count: usize,
+
+    /// Max number of torrents to store peers for
+    pub max_torrents: usize,
+
+    /// Max number of peers per torrent to store
+    pub max_peers_per_torrent: usize,
+
+    /// We'll think about pinging and pruning nodes at this interval
+    pub ping_check_interval_secs: u64,
+
+    /// Outgoing requests may be pruned after this many seconds
+    pub outgoing_request_prune_secs: u64,
+
+    /// We'll think about pruning outgoing requests at this interval
+    pub outgoing_reqiest_check_interval_secs: u64,
+}
+
+impl DHTSettings {
+    /// Returns DHTSettings with a default set of options.
+    pub fn default() -> DHTSettings {
+        DHTSettings {
+            token_secret_size: 10,
+            max_peers_response: 128,
+            max_sample_response: 50,
+            min_sample_interval_secs: 10,
+            router_ping_interval_secs: 900,
+            reverify_interval_secs: 14 * 60,
+            reverify_grace_period_secs: 15 * 60,
+            verify_grace_period_secs: 60,
+            get_peers_freshness_secs: 15 * 60,
+            find_nodes_interval_secs: 33,
+            find_nodes_skip_count: 32,
+            max_torrents: 50,
+            max_peers_per_torrent: 100,
+            ping_check_interval_secs: 10,
+            outgoing_request_prune_secs: 30,
+            outgoing_reqiest_check_interval_secs: 30,
+        }
+    }
+}
