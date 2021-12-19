92aa69d93bd358390b070988772660680df1232e
raptorswing <971991+raptorswing@users.noreply.github.com>
2021-12-18 18:48:30 -0800

    Cleanup and documentation (#40)
    * Fix unread property warning about shutdown_confirm_tx
    
    * Move DHT stuff into its own sub module
    
    * Cleanup and documentation

---
+++ src/dht/mod.rs
@@ -0,0 +1,9 @@
+mod dht;
+pub use dht::*;
+
+mod dht_settings;
+pub use dht_settings::*;
+
+pub mod dht_event;
+
+mod socket;
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 pub mod common;
 pub mod dht;
-pub mod dht_event;
 pub mod errors;
 pub mod packets;
 pub mod shutdown;
-pub mod socket;
 pub mod storage;
