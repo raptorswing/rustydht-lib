0fd387823fb7904b77c1e4159e6a1040501c3ae1
raptorswing <971991+raptorswing@users.noreply.github.com>
2021-12-30 17:13:46 -0800

    Cleanup operations::get_peers

--- src/dht/operations.rs
+++ src/dht/operations.rs
@@ -1,18 +1,19 @@
 use crate::common::Id;
 use crate::dht::DHT;
 use crate::packets;
+use crate::packets::MessageBuilder;
 use crate::storage::buckets::Buckets;
 use crate::storage::node_wrapper::NodeWrapper;
 use futures::StreamExt;
-use log::{debug, info, trace, warn};
+use log::{debug, error, info, trace, warn};
 use std::collections::HashSet;
 use std::net::SocketAddr;
 use std::time::{Duration, Instant};
 
 pub async fn get_peers(dht: &DHT, info_hash: Id, min_peers: usize) -> Vec<SocketAddr> {
     let mut to_ret = HashSet::new();
-
     let mut buckets = Buckets::new(info_hash, 8);
+    let dht_settings = dht.get_settings();
 
     while to_ret.len() < min_peers {
         // Prepopulate our buckets with the main buckets from the DHT
@@ -26,11 +27,22 @@ pub async fn get_peers(dht: &DHT, info_hash: Id, min_peers: usize) -> Vec<Socket
         let nearest = buckets.get_nearest_nodes(&info_hash, None);
 
         // Get ready to send get_peers to all of those closest nodes
+        let request_builder = MessageBuilder::new_get_peers_request()
+            .target(info_hash)
+            .read_only(dht_settings.read_only)
+            .sender_id(dht.get_id());
         let mut todos = futures::stream::FuturesUnordered::new();
         for node in nearest {
             let fut = tokio::time::timeout(
                 Duration::from_secs(10),
-                dht.send_get_peers(info_hash, node.node.address, Some(node.node.id)),
+                dht.send_request(
+                    request_builder
+                        .clone()
+                        .build()
+                        .expect("Failed to build get_peers request"),
+                    node.node.address,
+                    Some(node.node.id),
+                ),
             );
             todos.push(fut);
         }
@@ -38,36 +50,38 @@ pub async fn get_peers(dht: &DHT, info_hash: Id, min_peers: usize) -> Vec<Socket
         // Send get_peers to nearest nodes, handle their responses
         let started_sending_time = Instant::now();
         while let Some(timeout_result) = todos.next().await {
-            if timeout_result.is_err() {
-                debug!(target: "rustydht_lib::operations::get_peers", "got tokio timeout: {}", timeout_result.unwrap_err());
-                continue;
-            }
-            let get_peers_result = timeout_result.unwrap();
-            if let Err(e) = get_peers_result {
-                warn!(target: "rustydht_lib::operations::get_peers", "Encountered error during get_peers: {}", e);
-                continue;
-            }
-
-            if let packets::MessageType::Response(packets::ResponseSpecific::GetPeersResponse(
-                args,
-            )) = get_peers_result.unwrap().message_type
-            {
-                match args.values {
-                    packets::GetPeersResponseValues::Nodes(n) => {
-                        trace!(target: "rustydht_lib::operations::get_peers", "Got {} nodes", n.len());
-                        for node in n {
-                            if let None = buckets.get_mut(&node.id) {
-                                info!(target: "rustydht_lib::operations::get_peers", "Adding node {:?} to buckets", node);
-                                buckets.add(NodeWrapper::new(node), None);
+            match timeout_result {
+                Ok(request_result) => match request_result {
+                    Ok(message) => match message.message_type {
+                        packets::MessageType::Response(
+                            packets::ResponseSpecific::GetPeersResponse(args),
+                        ) => match args.values {
+                            packets::GetPeersResponseValues::Nodes(n) => {
+                                debug!(target: "rustydht_lib::operations::get_peers", "Got {} nodes", n.len());
+                                for node in n {
+                                    if let None = buckets.get_mut(&node.id) {
+                                        trace!(target: "rustydht_lib::operations::get_peers", "Adding node {:?} to buckets", node);
+                                        buckets.add(NodeWrapper::new(node), None);
+                                    }
+                                }
                             }
+                            packets::GetPeersResponseValues::Peers(p) => {
+                                info!(target: "rustydht_lib::operations::get_peers", "Got {} peers", p.len());
+                                for peer in p {
+                                    to_ret.insert(peer);
+                                }
+                            }
+                        },
+                        _ => {
+                            error!(target: "rustydht_lib::operations::get_peers", "Got wrong packet type back: {:?}", message);
                         }
+                    },
+                    Err(e) => {
+                        warn!(target: "rustydht_lib::operations::get_peers", "Error sending get_peers request: {}", e);
                     }
-                    packets::GetPeersResponseValues::Peers(p) => {
-                        trace!(target: "rustydht_lib::operations::get_peers", "Got {} peers", p.len());
-                        for peer in p {
-                            to_ret.insert(peer);
-                        }
-                    }
+                },
+                Err(e) => {
+                    debug!(target: "rustydht_lib::operations::get_peers", "get_peers request timed out: {}", e);
                 }
             }
         }
