3eb1c4d3e6672e26a42ca5e9fb5662f0d9446445
raptorswing <971991+raptorswing@users.noreply.github.com>
2022-01-09 11:04:43 -0800

    Document NodeWrapper

--- src/storage/node_wrapper.rs
+++ src/storage/node_wrapper.rs
@@ -2,15 +2,28 @@ use super::buckets::Bucketable;
 use crate::common::{Id, Node};
 use std::time::Instant;
 
+/// Wraps a Node with information about when the DHT first saw, last saw it, and last verified it.
+/// This is used by [NodeStorage](crate::storage::node_bucket_storage::NodeStorage) implementations
 #[derive(Debug, Clone)]
 pub struct NodeWrapper {
     pub node: Node,
+
+    /// The Instant when this Node was first seen by us on the network
     pub first_seen: std::time::Instant,
+
+    /// The Instant when this Node was last marked seen by us on the network.
     pub last_seen: std::time::Instant,
+
+    /// The Instant when this Node was most recently marked as being alive on the network.
+    /// In general, being "verified" requires that the Node has responded to a request
+    /// from us recently.
     pub last_verified: Option<std::time::Instant>,
 }
 
 impl NodeWrapper {
+    /// Creates a new NodeWrapper.
+    ///
+    /// Initializes `first_seen` and `last_seen` to the current time. Initializes `last_verified` to `None`.
     pub fn new(node: Node) -> NodeWrapper {
         let now = std::time::Instant::now();
         NodeWrapper {
